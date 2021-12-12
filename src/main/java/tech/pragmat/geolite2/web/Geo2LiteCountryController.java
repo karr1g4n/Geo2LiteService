571965de513f96ab4a4192039c99d8284c5c2fa8
spring security <licherep803@gmail.com>
2021-12-12 16:22:45 +0300

    change code

--- src/main/java/tech/pragmat/geolite2/web/Geo2LiteCountryController.java
+++ src/main/java/tech/pragmat/geolite2/web/Geo2LiteCountryController.java
@@ -2,8 +2,13 @@ package tech.pragmat.geolite2.web;
 
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import tech.pragmat.geolite2.service.GeoLite2CountryService;
+
 import java.io.IOException;
 
 @Slf4j
@@ -13,15 +18,13 @@ public class Geo2LiteCountryController {
 
     private final GeoLite2CountryService geoLite2CountryService;
 
-    public Geo2LiteCountryController( GeoLite2CountryService geoLite2CountryService){
-        this.geoLite2CountryService=geoLite2CountryService;
+    public Geo2LiteCountryController(GeoLite2CountryService geoLite2CountryService) {
+        this.geoLite2CountryService = geoLite2CountryService;
     }
 
     @GetMapping("/getCountryByIp")
     public String getCountryName(@RequestParam String ip) throws IOException, GeoIp2Exception {
         log.info("try get name of country by ip");
-       return   geoLite2CountryService.getNameOfCountry(ip);
+        return geoLite2CountryService.getNameOfCountry(ip);
     }
-
-
 }
