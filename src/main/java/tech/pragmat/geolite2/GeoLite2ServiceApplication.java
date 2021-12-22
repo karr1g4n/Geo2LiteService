d3efbd2356c3420781f2f823ae81e60468148cff
Karr1g4n <licherep803@gmail.com>
2021-12-22 14:21:42 +0200

    ribbon configuration

--- src/main/java/tech/pragmat/geolite2/GeoLite2ServiceApplication.java
+++ src/main/java/tech/pragmat/geolite2/GeoLite2ServiceApplication.java
@@ -2,9 +2,9 @@ package tech.pragmat.geolite2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class GeoLite2ServiceApplication {
 
